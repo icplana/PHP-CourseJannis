0988cfeaf464852265f5c71b568600959ff12056
Ignasi CP <116747620+icplana@users.noreply.github.com>
2024-11-06 21:37:14 +0100

    a bit more

---
+++ phpBasis/strings.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="display: flex; justify-content: center; font-size: 2rem; ">
+    <pre>
+        <?php
+
+        $text = 'i\'m a string' . ' concat';
+        echo "{$text}_";
+
+        echo "sometet";
+        echo "\n";
+        echo "moretext";
+        echo "</br>";
+        var_dump(123);
+        var_dump(42.42);
+        var_dump("teddst">1000);
+        ?>
+    </pre>
+</body>
+</html>
